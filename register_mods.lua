88787bcc525781a19aebaa9620ee74950c1cb3dc
acmgit <undertakers_help@yahoo.com>
2025-10-23 10:02:56 +0200

    Registering blocks @ technic_cnc.

--- register_mods.lua
+++ register_mods.lua
@@ -2,6 +2,7 @@
 
 local cst = colored_stone
 local colors = cst.colors
+local modname = cst.modname
 
 if(minetest.get_modpath("moreblocks")) then
 --[[
@@ -15,37 +16,63 @@ stairsplus:register_all("moreblocks", "wood", "default:wood", {
 
     for _,color in pairs(colors) do
         stairsplus:register_all("moreblocks", "cobble_" .. color[1], "colored_stone:cobble_" .. color[1],{
-                description = minetest.registered_nodes["colored_stone:cobble_" .. color[1]].description,
-                tiles = minetest.registered_nodes["colored_stone:cobble_" .. color[1]].tiles,
-                groups = minetest.registered_nodes["colored_stone:cobble_" .. color[1]].groups,
-                sounds = minetest.registered_nodes["colored_stone:cobble_" .. color[1]].sounds,
+                description = minetest.registered_nodes[modname .. ":cobble_" .. color[1]].description,
+                tiles = minetest.registered_nodes[modname .. ":cobble_" .. color[1]].tiles,
+                groups = minetest.registered_nodes[modname .. ":cobble_" .. color[1]].groups,
+                sounds = minetest.registered_nodes[modname .. ":cobble_" .. color[1]].sounds,
 
         })
 
         stairsplus:register_all("moreblocks", "stone_" .. color[1], "colored_stone:stone_" .. color[1],{
-                description = minetest.registered_nodes["colored_stone:stone_" .. color[1]].description,
-                tiles = minetest.registered_nodes["colored_stone:stone_" .. color[1]].tiles,
-                groups = minetest.registered_nodes["colored_stone:stone_" .. color[1]].groups,
-                sounds = minetest.registered_nodes["colored_stone:stone_" .. color[1]].sounds,
+                description = minetest.registered_nodes[modname .. ":stone_" .. color[1]].description,
+                tiles = minetest.registered_nodes[modname .. ":stone_" .. color[1]].tiles,
+                groups = minetest.registered_nodes[modname .. ":stone_" .. color[1]].groups,
+                sounds = minetest.registered_nodes[modname .. ":stone_" .. color[1]].sounds,
 
         })
 
         stairsplus:register_all("moreblocks", "stone_" .. color[1], "colored_stone:stonebrick_" .. color[1],{
-                description = minetest.registered_nodes["colored_stone:stonebrick_" .. color[1]].description,
-                tiles = minetest.registered_nodes["colored_stone:stonebrick_" .. color[1]].tiles,
-                groups = minetest.registered_nodes["colored_stone:stonebrick_" .. color[1]].groups,
-                sounds = minetest.registered_nodes["colored_stone:stonebrick_" .. color[1]].sounds,
+                description = minetest.registered_nodes[modname .. ":stonebrick_" .. color[1]].description,
+                tiles = minetest.registered_nodes[modname .. ":stonebrick_" .. color[1]].tiles,
+                groups = minetest.registered_nodes[modname .. ":stonebrick_" .. color[1]].groups,
+                sounds = minetest.registered_nodes[modname .. ":stonebrick_" .. color[1]].sounds,
 
         })
 
-        stairsplus:register_all("moreblocks", "stone_" .. color[1], "colored_stone:stone_block_" .. color[1],{
-                description = minetest.registered_nodes["colored_stone:stone_block_" .. color[1]].description,
-                tiles = minetest.registered_nodes["colored_stone:stone_block_" .. color[1]].tiles,
-                groups = minetest.registered_nodes["colored_stone:stone_block_" .. color[1]].groups,
-                sounds = minetest.registered_nodes["colored_stone:stone_block_" .. color[1]].sounds,
+        stairsplus:register_all("moreblocks", "stone_" .. color[1], modname .. ":stone_block_" .. color[1],{
+                description = minetest.registered_nodes[modname .. ":stone_block_" .. color[1]].description,
+                tiles = minetest.registered_nodes[modname .. ":stone_block_" .. color[1]].tiles,
+                groups = minetest.registered_nodes[modname .. ":stone_block_" .. color[1]].groups,
+                sounds = minetest.registered_nodes[modname .. ":stone_block_" .. color[1]].sounds,
 
         })
 
     end
+
+end
+
+if(minetest.get_modpath("technic_cnc")) then
+
+        for _,color in pairs(colors) do
+                technic_cnc.register_all(modname .. ":cobble_" .. color[1],
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].groups,
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].tiles,
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].description)
+
+                technic_cnc.register_all(modname .. ":stone_" .. color[1],
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].groups,
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].tiles,
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].description)
+
+                technic_cnc.register_all(modname .. ":stonebrick_" .. color[1],
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].groups,
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].tiles,
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].description)
+
+                technic_cnc.register_all(modname .. ":stoneblock_" .. color[1],
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].groups,
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].tiles,
+                                        minetest.registered_nodes[modname .. ":cobble_" .. color[1]].description)
+        end
 
 end
